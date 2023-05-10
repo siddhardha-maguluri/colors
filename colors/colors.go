c7fd775cedde8612b59f2c9752c6a5a6a6a489a3
Sai Siddhardha Maguluri <Siddhardha.Maguluri@ou.edu>
2023-05-09 23:53:00 -0500

    code update

--- colors/colors.go
+++ colors/colors.go
@@ -7,14 +7,17 @@ type Log struct{}
 // Use to display the error messages in red color
 func (l *Log) Error(message string) {
 	fmt.Printf("%s%s%s", TEXT_COLORS["RED"], message, TEXT_COLORS["BLACK"])
+	fmt.Println()
 }
 
 // Use to display the warning messages in yellow color
 func (l *Log) Warn(message string) {
 	fmt.Printf("%s%s%s", TEXT_COLORS["YELLOW"], message, TEXT_COLORS["BLACK"])
+	fmt.Println()
 }
 
 // Use to display the info messages in blue color
 func (l *Log) Info(message string) {
 	fmt.Printf("%s%s%s", TEXT_COLORS["BLUE"], message, TEXT_COLORS["BLACK"])
+	fmt.Println()
 }
